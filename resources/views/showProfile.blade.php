da369d7ec881c4ddda62bdeb3b76f57912ddd20c
gytdau <gytdau@gmail.com>
2016-02-01 19:40:12 +0000

    Restyled profile page

--- resources/views/showProfile.blade.php
+++ resources/views/showProfile.blade.php
@@ -1,1 +1,12 @@
-<h1>{{ $user->name }}</h1>
+@extends('template')
+
+@section('body-class')
+    dark
+@endsection
+@section('content')
+    <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default shadow-2 profile-panel">
+            <h1 class="text-center">{{ $user->name }}</h1>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/template.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Identifyi</title>
+        <link rel="stylesheet" href="https://bootswatch.com/flatly/bootstrap.min.css">
+        <link rel="stylesheet" href="/shadows.css">
+    </head>
+    <body class="@yield('body-class')">
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
