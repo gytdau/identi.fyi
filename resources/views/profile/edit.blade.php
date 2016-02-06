94ae1a2ce84a072b62b9ad3fb92b7bcacb511e89
Ciaran Flanagan <ciaran@cicisoft.net>
2016-02-06 21:57:47 +0000

    Created UI For Edit Form
    Created the UI for the edit form. NOTE: Form doesn’t do anything upon
    completion yet

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -6,8 +6,21 @@
 @section('content')
     <div class="col-md-8 col-md-offset-2">
         <div class="panel panel-default shadow-2 profile-panel">
-            <h1 class="text-center">Editing your profile</h1>
-            <h3>We'll put something here eventually</h3>
+            <h1 class="text-center">Editing Your Profile</h1>
+            <h4 class="text-center">( Profile Of {{ $user->name }} )</h4>
+			<form>
+	<!-- Textarea Input Type -->
+	            <label>
+	        		Bio
+	            </label>
+	            <i><textarea name="bio" class="form-control">{{ $user->bio }}</textarea></i><br>
+	<!-- Text Input Type -->
+	            <label>
+	        		Email
+	            </label><br>
+	            <i><input type="text" class="form-control" value="{{ $user->email }}"><br><br></i>
+	            <input type="submit" value="Save Changes" class="form-control">
+	        </form>
         </div>
     </div>
 @endsection
