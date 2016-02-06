f03219574e19913530ae32a2265676a6893973f0
gytdau <gytdau@gmail.com>
2016-02-06 18:26:39 +0000

    Added incomplete editing
    I also added an error template, which was lazily modified from the 'Welcome to Laravel 5' template originally included.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,24 +11,33 @@ use App\User;
 
 class UserController extends Controller
 {
-    public function showProfile($name, $code) {
+    public function showProfile($name, $code)
+    {
 		
         $userFound = User::where('url', $name)->where('code', $code)->firstOrFail();
-        //return response()->json($userFound);
-        return view('showProfile')->with('user', $userFound);
+        return view('profile.show')->with('user', $userFound);
     }
-	
-	public function signup(Request $request){
+
+    public function edit($name, $code, $passcode)
+    {
+        $userFound = User::where('url', $name)->where('code', $code)->firstOrFail();
+        if($userFound->passcode != $passcode) {
+            return view('errors.custom')->with('text', "This link has expired. Check your inbox for a new email, or try sending it again.");
+        }
+        return view('profile.edit')->with('user', $userFound);
+    }
+	public function signup(Request $request)
+    {
 		
-		 $email = $request->input('email');
-		 $name = $request->input('name');
+        $email = $request->input('email');
+        $name = $request->input('name');
 		 
-		 $user = new User;
-            $user->email = $email;
-			$user->name = $name;
-            $user->generateUrl();
-            $user->generateCode();
-            $user->save();
+        $user = new User;
+        $user->email = $email;
+        $user->name = $name;
+        $user->generateUrl();
+        $user->generateCode();
+        $user->save();
 		
 	}
 	
---
+++ resources/views/profile/edit.blade.php
@@ -0,0 +1,13 @@
+@extends('template')
+
+@section('body-class')
+    dark
+@endsection
+@section('content')
+    <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default shadow-2 profile-panel">
+            <h1 class="text-center">Editing your profile</h1>
+            <h3>We'll put something here eventually</h3>
+        </div>
+    </div>
+@endsection
