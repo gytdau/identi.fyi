28b945f0ece16ddcfeea20af7be61cd2acaa04e2
gytdau <gytdau@gmail.com>
2016-02-07 14:24:18 +0000

    Added full URL output to seeding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,50 @@
 @extends('template')
 
 @section('head')
-	
-	<title>Identifyi - Your Virtual Business Card</title>
+
+    <title>Identifyi - Your Virtual Business Card</title>
 
     <link rel="stylesheet" href="/indexstyle.css">
-	
+
 @endsection
 
 @section('content')
     <div class="row">
-            <div>
-                <h1 class="text-center">Identi.fyi</h1>
-            </div>   
+        <div>
+            <h1 class="text-center">Identi.fyi</h1>
+        </div>
         <div class="col-md-7">
             <div class="panel panel-default shadow-2 profile-panel">
                 <h1 class="text-center">Juliet Capulet</h1>
-                <div class="page-card bio"><div class="text-muted">About me</div>I love pottery and gardening.</div>
-                <div class="page-card email"><div class="text-muted">Email</div><a href="mailto: juliet@capulet.com">juliet@capulet.com</a></div>
-                <div class="page-card twitter"><div class="text-muted">Twitter</div><a href="https://twitter.com/julietcapulet">@julietcapulet</a></div>
-                <div class="page-card linkedin"><div class="text-muted">LinkedIn</div><a href="https://www.linkedin.com/in/julietcapulet">www.linkedin.com/in/julietcapulet</a></div>
+                <div class="page-card bio">
+                    <div class="text-muted">About me</div>
+                    I love pottery and gardening.
+                </div>
+                <div class="page-card email">
+                    <div class="text-muted">Email</div>
+                    <a href="mailto: juliet@capulet.com">juliet@capulet.com</a>
+                </div>
+                <div class="page-card twitter">
+                    <div class="text-muted">Twitter</div>
+                    <a href="https://twitter.com/julietcapulet">@julietcapulet</a>
+                </div>
+                <div class="page-card linkedin">
+                    <div class="text-muted">LinkedIn</div>
+                    <a href="https://www.linkedin.com/in/julietcapulet">www.linkedin.com/in/julietcapulet</a>
+                </div>
             </div>
         </div>
         <div class="col-md-5">
-        <h1>Your virtual business card</h1>
-        <h2>Get started in one click</h2>
-        <form class="signupform" method="post" action="">
-            <div class="input-group">
-                <input type="text" class="form-control" name="email" placeholder="Your email address">
+            <h1>Your virtual business card</h1>
+            <h2>Get started in one click</h2>
+            <form class="signupform" method="post" action="">
+                <div class="input-group">
+                    <input type="text" class="form-control" name="email" placeholder="Your email address">
                         <span class="input-group-btn">
                             <input class="btn btn-danger" type="submit" value="Sign up">
                         </span>
-            </div><br><br>
-            <div class="temporary-links">
-                <p>
-                    <h3>Temporary Links</h3>
-                    <h4>Makes it quicker to get to the first profile</h4><br>
-                    <a href="/dr-adriel-cruickshank/4NXe">/dr-adriel-cruickshank/4NXe</a><br><br>
-                    <a href="/dr-adriel-cruickshank/4NXe/edit/1e8D3oVlVopOFdLs3MnD">/dr-adriel-cruickshank/4NXe/edit/1e8D3oVlVopOFdLs3MnD</a>
-                </p>
-            </div>
-        </form>
+                </div>
+            </form>
         </div>
         <div class="col-md-12">
             <div class="panel panel-default shadow-2 profile-panel">
